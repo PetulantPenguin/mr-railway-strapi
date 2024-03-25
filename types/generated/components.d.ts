8f6ea660aa4963904e19cbe3fc4281ece2c4b5aa
jrcowan3 <jimmy@stitchedin.com>
2024-03-25 08:14:04 -0500

    feat: add new types

---
+++ types/generated/components.d.ts
@@ -0,0 +1,87 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface ComponentsFeature extends Schema.Component {
+  collectionName: 'components_components_features';
+  info: {
+    displayName: 'Feature';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    icon: Attribute.Enumeration<['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']>;
+  };
+}
+
+export interface ComponentsLink extends Schema.Component {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'link';
+  };
+  attributes: {
+    url: Attribute.String;
+    text: Attribute.String;
+    isExternal: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface LayoutFeatureSection extends Schema.Component {
+  collectionName: 'components_layout_feature_sections';
+  info: {
+    displayName: 'Feature Section';
+    description: '';
+  };
+  attributes: {
+    title: Attribute.String;
+    description: Attribute.Text;
+    feature: Attribute.Component<'components.feature', true>;
+  };
+}
+
+export interface LayoutFooter extends Schema.Component {
+  collectionName: 'components_layout_footers';
+  info: {
+    displayName: 'Footer';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    text: Attribute.String;
+    socialLinks: Attribute.Component<'components.link', true>;
+  };
+}
+
+export interface LayoutHeader extends Schema.Component {
+  collectionName: 'components_layout_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    logoText: Attribute.Component<'components.link'>;
+    ctabutton: Attribute.Component<'components.link'>;
+  };
+}
+
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    image: Attribute.Media;
+    link: Attribute.Component<'components.link'>;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'components.feature': ComponentsFeature;
+      'components.link': ComponentsLink;
+      'layout.feature-section': LayoutFeatureSection;
+      'layout.footer': LayoutFooter;
+      'layout.header': LayoutHeader;
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
+}
